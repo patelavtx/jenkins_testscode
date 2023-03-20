b8ed8fc09b40577e71c17534cce6a9925821aa12
patelavtx <apatel@aviatrix.com>
2023-03-20 20:22:27 +0000

    first

---
+++ testscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "this script is from github"
+NUMBER=$(($NUMBERONE + $NUMBERTWO))
+echo "$NUMBER"
+echo "This was added"
+echo "Testing using github server PAT connection and hook to this job"
+
